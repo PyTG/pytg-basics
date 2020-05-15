dc7519634c00bf9de5954aafd6558ec092f3070e
Lorenzo Catania <aegroto@protonmail.com>
2020-05-15 17:52:27 +0200

    dummy request digester compliant to new arguments passed by the digest manager

--- various_digesters/src/digesters/requests/dummy.py
+++ various_digesters/src/digesters/requests/dummy.py
@@ -4,5 +4,5 @@ from datetime import datetime
 
 from modules.pytg.ModulesLoader import ModulesLoader
 
-def dummy_request_digester(bot, chat_id, request_data, response):
-    logging.info("Dummy request digesting ({}, {}, {})".format(chat_id, request_data, response))
+def dummy_request_digester(bot, chat_id, message_id, request_id, request_data, response):
+    logging.info("Dummy request digesting ({}, {}, {}, {}, {})".format(chat_id, message_id, request_id, request_data, response))

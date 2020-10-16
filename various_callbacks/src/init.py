26f1de21040cc8ab6334e408491ed1ed56e8024f
aegroto <aegroto@protonmail.com>
2020-10-16 16:05:27 +0200

    fixed various callbacks init file

--- various_callbacks/src/init.py
+++ various_callbacks/src/init.py
@@ -1,22 +1,19 @@
 import telegram, logging
 
-from modules.pytg.ModulesLoader import ModulesLoader
+from modules.pytg.load import manager
 
 from telegram.ext import CallbackQueryHandler
 
+from .handlers.callbacks.dummy import dummy_callback_handler
+
 def load_callback_handlers(dispatcher):
-    pass
+    dispatcher.add_handler(CallbackQueryHandler(dummy_callback_handler, "dummy,*"))
 
 def initialize():
-    logging.info("Initializing your_bot_callbacks module...")
     pass
 
 def connect():
-    logging.info("Connecting your_bot_callbacks module...")
-
-    bot_manager = ModulesLoader.load_manager("bot")
-
-    load_callback_handlers(bot_manager.updater.dispatcher)
+    load_callback_handlers(manager("bot").updater.dispatcher)
 
 def load_manager():
     # There is no need for a manager in this package
